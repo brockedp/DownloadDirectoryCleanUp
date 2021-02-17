bd562ff557f8690fde49aa87601b3dee94d3d3f6
Deno Brockert <dbrockert@gsic.local>
2021-02-17 11:12:44 -0500

    First push

---
+++ DirectoryCleanup/DirectoryCleanup.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DirectoryCleanup
+{
+    public class DirectoryCleanup
+    {
+        public DirectoryCleanup()
+        {
+        }
+
+        public void Start()
+        {
+            //TODO - figure out what will run on the constructor
+            //TODO - Start service
+            //TODO - stop service
+            try
+            {
+                System.IO.DirectoryInfo di = new DirectoryInfo(@"C:\Users\dbrockert\Downloads");
+                if (di.Exists)
+                {
+                    Console.WriteLine("---------------Files--------------------");
+                    foreach (FileInfo file in di.GetFiles())
+                    {
+                        Console.WriteLine($"{file.Name} is going to be deleted. ");
+                        file.Delete();
+                    }
+                    Console.WriteLine("---------------Directory--------------------");
+                    foreach (DirectoryInfo dir in di.GetDirectories())
+                    {
+                        Console.WriteLine($"{dir.Name} is going to be deleted.");
+                        dir.Delete(true);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine(ex);
+            }
+            
+
+        }
+        public void Stop()
+        {
+
+        }
+    }
+}
---
+++ DirectoryCleanup/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Topshelf;
+
+namespace DirectoryCleanup
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var exitCode = HostFactory.Run(x =>
+            {
+                x.Service<DirectoryCleanup>(s =>
+                {
+                    s.ConstructUsing(directoryCleanup => new DirectoryCleanup());
+                    s.WhenStarted(directoryCleanup => directoryCleanup.Start());
+                    s.WhenStopped(directoryCleanup => directoryCleanup.Stop());
+                    
+                    
+                });
+                x.StartManually();
+                x.RunAsLocalSystem();
+                x.SetServiceName("DownloadDirectoryCleanup");
+                x.SetDisplayName("Download Directory Cleanup");
+                x.SetDescription("This service deletes all files and directories in the Downloads folder");
+            });
+            int exitCodeValue = (int)Convert.ChangeType(exitCode, exitCode.GetTypeCode());
+            Environment.ExitCode = exitCodeValue;
+
+        }
+    }
+    
+}
